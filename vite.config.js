b1a06f179dc38e68876d690ca2c61dc42e64454a
Chamce <chancellor.whitaker@eku.edu>
2023-08-04 23:57:15 -0400

    first commit

---
+++ vite.config.js
@@ -0,0 +1,38 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import babel from 'vite-plugin-babel';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    react(),
+    babel({
+      babelConfig: {
+        babelrc: false,
+        configFile: false,
+        plugins: ['styled-jsx/babel'],
+      },
+    }),
+  ],
+  build: {
+    lib: {
+      formats: ['cjs'],
+      entry: resolve(__dirname, 'src/remoteEntry.jsx'),
+    },
+    rollupOptions: {
+      external: [
+        'react',
+        'react-dom',
+        '@paciolan/remote-component',
+        'bootstrap',
+        'styled-jsx',
+      ],
+      output: { exports: 'named' },
+    },
+  },
+  define:
+    mode === 'production'
+      ? { 'process.env.NODE_ENV': '"production"' }
+      : {},
+}));
