6858084121ffe53df54d98ad6e07e2160cdbd9b0
Chamce <chancellor.whitaker@eku.edu>
2023-08-10 22:20:44 -0400

    fold style

--- src/components/Fold.jsx
+++ src/components/Fold.jsx
@@ -3,12 +3,9 @@ const Fold = () => {
     <div className="position-absolute top-0 start-0 d-none d-sm-block" id="corner-absolute-position">
       <div className="position-relative folded-corner" id="corner-container">
         <div className="z-1 h-100 position-absolute bottom-100 end-0 hypotenuse bg-picker" id="fold-hypotenuse">
-          <div className="position-relative w-100 h-50 bg-picker" id="hypotenuse-excess-shadow-cover"></div>
+          <div className="position-relative w-100 h-75 bg-picker" id="hypotenuse-excess-shadow-cover"></div>
         </div>
-        <div
-          className="z-1 h-100 w-100 position-absolute bottom-100 end-0 bg-picker rounded-end-1"
-          id="above-corner"
-        ></div>
+        <div className="z-1 h-100 w-100 position-absolute bottom-100 end-0 bg-picker" id="above-corner"></div>
         <div
           className="z-1 h-100 w-100 position-absolute rotate-negative-90-bottom-left bg-picker rounded-bottom-1"
           id="left-of-corner"
