c66ded91ce05f757ce6b87576a25bb3a206fa58e
Chamce <chancellor.whitaker@eku.edu>
2023-08-11 13:19:41 -0400

    changed border radius on fold inset shadow

--- src/components/Fold.jsx
+++ src/components/Fold.jsx
@@ -7,7 +7,7 @@ const Fold = () => {
         </div>
         <div className="z-1 h-100 w-100 position-absolute bottom-100 end-0 bg-picker" id="above-corner"></div>
         <div
-          className="z-1 h-100 w-100 position-absolute rotate-negative-90-bottom-left bg-picker rounded-bottom-1"
+          className="z-1 h-100 w-100 position-absolute rotate-negative-90-bottom-left bg-picker rounded-bottom"
           id="left-of-corner"
         ></div>
       </div>
