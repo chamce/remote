f39c682fca0dfd82d526c946b0b99911bf0adaf4
chamce <chancellor.whitaker@eku.edu>
2025-01-22 11:22:59 -0500

    sidebar links external

--- src/Remote/components/Sidebar/index.jsx
+++ src/Remote/components/Sidebar/index.jsx
@@ -1,13 +1,17 @@
+import { useData } from "../../../hooks/useData";
 import { NavLink } from "./NavLink";
-import { lists } from "./lists";
 
 export const Sidebar = () => {
+  const lists = useData(
+    "https://raw.githubusercontent.com/chancellor-w-whitaker/json-example/refs/heads/main/sidebar.json",
+  );
+
   return (
     <div className="sidebar col-md-3 col-lg-2 p-0 bg-body-tertiary material-shadow-lg">
       <div aria-labelledby="sidebarMenuLabel" className="bg-body-tertiary" id="sidebarMenu" tabIndex={-1}>
         <div className="offcanvas-body d-md-flex flex-column p-0 py-lg-3 overflow-y-auto">
           <div className="vstack gap-3">
-            {lists.map(({ heading, items }) => (
+            {lists?.map(({ heading, items }) => (
               <div key={heading}>
                 <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-2 mb-1 text-body-secondary text-uppercase">
                   <span>{heading}</span>
